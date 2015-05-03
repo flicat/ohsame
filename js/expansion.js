83d12e4d68f9fa65a666457cdeb7f91594bfdac5
flicat <liyuelong1020@gmail.com>
2015-05-03 21:00:32 +0800

    modify script

--- js/expansion.js
+++ js/expansion.js
@@ -44,15 +44,71 @@ define(function(require, exports, module) {
                 body: $('#role-4-body')
             }
         },
-        roleIconList: {
-            icon_1: $('#role-icon-1'),   
-            icon_2: $('#role-icon-2'),   
-            icon_3: $('#role-icon-3'),   
-            icon_4: $('#role-icon-4')   
-        }
+        roleIconList: [
+            $('#role-icon-1'),
+            $('#role-icon-2'),
+            $('#role-icon-3'),
+            $('#role-icon-4')
+        ]
     };
 
+    /**
+     * @param {Node} background
+     * @param {Node} cloud
+     * @returns {Object} 场景对象
+     * @description 场景
+     */
+    var Scene = new scene.Scene(chimneyNodes.chimney, chimneyNodes.cloud);
 
+    /**
+     * @param {Node} role
+     * @param {Node} head
+     * @param {Node} neck
+     * @param {Node} body
+     * @returns {Object} 角色对象
+     * @description 角色列表
+     */
+    var roleList = [
+        new scene.Role(
+            chimneyNodes.roleList.role_1.role,
+            chimneyNodes.roleList.role_1.head,
+            chimneyNodes.roleList.role_1.neck,
+            chimneyNodes.roleList.role_1.body
+        ),
+        new scene.Role(
+            chimneyNodes.roleList.role_2.role,
+            chimneyNodes.roleList.role_2.head,
+            chimneyNodes.roleList.role_2.neck,
+            chimneyNodes.roleList.role_2.body),
+        new scene.Role(
+            chimneyNodes.roleList.role_3.role,
+            chimneyNodes.roleList.role_3.head,
+            chimneyNodes.roleList.role_3.neck,
+            chimneyNodes.roleList.role_3.body),
+        new scene.Role(
+            chimneyNodes.roleList.role_4.role,
+            chimneyNodes.roleList.role_4.head,
+            chimneyNodes.roleList.role_4.neck,
+            chimneyNodes.roleList.role_4.body)
+    ];
+
+    /**
+     * @param {[Role]} roleArr
+     * @param {NodeList} roleIconNodeArr
+     * @param {Node} roleList
+     * @returns {Object} 角色切换方法
+     * @description 角色切换
+     */
+    var RoleSwitch = new scene.RoleSwitch(roleList, chimneyNodes.roleIconList, roleList);
+
+    /**
+     * @param {Scene} scene
+     * @param {RoleSwitch} role
+     * @returns {Object} 拉长脖子对象
+     * @description 拉长脖子
+     * @constructs
+     */
+    var Extension = new scene.Extension(Scene, RoleSwitch);
 
 });
 
--- js/scene.js
+++ js/scene.js
@@ -6,11 +6,14 @@
  */
 
 define(function(require, exports, module) {
+    var zepto = require('zepto');
 
     /** @description 场景宽度 */
     var winWidth = window.innerWidth;
     /** @description 场景高度 */
     var winHeight = window.innerHeight;
+    /** @description 头距离屏幕上方的高度 */
+    var offsetTop = 0;
 
     /**
      * @param {Node} background
@@ -26,9 +29,16 @@ define(function(require, exports, module) {
     };
     Scene.prototype = {
         constructor: Scene,
-        // 切换背景
+        // 更新背景高度
         scrollBackground: function(metre) {
-
+            metre -= offsetTop;
+            if(metre < 0){
+                metre = 0;
+            }
+            this.background.css({
+                '-webkit-transform': 'translateY(' + metre + 'px)',
+                'transform': 'translateY(' + metre + 'px)'
+            });
         },
         // 初始化白云
         initCloud: function() {
@@ -37,33 +47,72 @@ define(function(require, exports, module) {
     };
 
     /**
+     * @param {Node} role
      * @param {Node} head
      * @param {Node} neck
+     * @param {Node} body
      * @returns {Object} 角色对象
      * @description 角色
      * @constructs
      */
-    var Role = function(head, neck) {
-        this.head = head;        // 头
-        this.neck = neck;        // 脖子
+    var Role = function(role, head, neck, body) {
+        this.role = role;                     // 全身
+        this.head = head;                     // 头
+        this.neck = neck;                     // 脖子
+        this.body = body;                     // 身体
+        this.neckHeight = neck.height();      // 脖子高度
+        this.offsetTop = role.offset().top - 100;   // 头距离屏幕上方的高度
+    };
+    // 脖子拉长方法
+    Role.prototype.expansion = function(interval) {
+        // 脖子伸缩量
+        var scaleDiff = interval / this.neckHeight;
+        if(scaleDiff <= 0){
+            scaleDiff = 1;
+        }
+        // 头位移
+        var translateDiff = interval - this.neckHeight;
+        if(scaleDiff === 1) {
+            translateDiff = 0;
+        }
+//        console.log(interval, this.neckHeight, scaleDiff);
+
+        // 头部上移
+        this.head.css({
+            '-webkit-transform': 'translateY(-' + translateDiff + 'px)',
+            'transform': 'translateY(-' + translateDiff + 'px)'
+        });
+        // 伸缩脖子
+        this.neck.css({
+            '-webkit-transform': 'scaleY(' + scaleDiff + ')',
+            'transform': 'scaleY(' + scaleDiff + ')'
+        });
+
     };
 
     /**
      * @param {[Role]} roleArr
+     * @param {NodeList} roleIconNodeArr
+     * @param {Node} roleList
      * @returns {Object} 角色切换方法
      * @description 角色切换
      * @constructs
      */
-    var RoleSwitch = function(roleArr) {
-        this.roleArr = roleArr;         // 角色列表
-        this.currentRole = roleArr[0];  // 当前角色
+    var RoleSwitch = function(roleArr, roleIconNodeArr, roleList) {
+        this.roleArr = roleArr;             // 角色列表
+        this.iconArr = roleIconNodeArr;     // 角色切换按钮列表
+        this.roleList = roleList;     // 角色切换 ul 节点
+        this.currentRole = roleArr[0];      // 当前角色
+        offsetTop = this.currentRole.offsetTop;   // 头距离屏幕上方的高度
     };
     RoleSwitch.prototype = {
         constructor: RoleSwitch,
         // 切换角色
-
+        switchRole: function() {},
         // 绑定点击事件
-        init: function() {}
+        init: function() {
+
+        }
     };
 
     /**
@@ -76,16 +125,74 @@ define(function(require, exports, module) {
     var Extension = function(scene, role) {
         this.scene = scene;        // 场景
         this.role = role;        // 角色列表
+        this.initHeight = 0;     // 初始化缩放高度
+        this.initPointY = 0;
+        this.init();
     };
     Extension.prototype = {
         constructor: Extension,
+
+        // 更新动画
+        update: function() {
+            // 计算高度
+            var interval = parseInt(Math.sqrt(Math.abs(this.initHeight)) || 0);
+            interval *= this.initHeight > 0 ? 10 : -10;
+
+            // 拉长脖子
+            this.role.currentRole.expansion(interval);
+            // 场景更新
+            this.scene.scrollBackground(interval);
+        },
+
+        // 初始化偏移量
+        clearHeight: function() {
+            this.initPointY = 0;
+            this.initHeight = 0;
+            this.update();
+        },
+
         // 绑定触摸事件
-        init: function() {}
+        init: function() {
+            var that = this;
+            // 触摸延时清除
+            var timer = null;
+            // 绑定触摸事件
+            $(document).on('touchstart touchmove touchend touchcancel', function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+//                console.log(e);
+
+                // 触摸坐标点
+                var points = e.touches[0];
+
+                switch(e.type) {
+                    case 'touchstart':
+//                        clearTimeout(timer);
+                        that.initPointY = points.clientY;
+                        break;
+                    case 'touchmove':
+                        that.initHeight += that.initPointY - points.clientY;
+                        that.initPointY = points.clientY;
+                        break;
+                    case 'touchend':
+                    case 'touchcancel':
+//                        console.log(that.initHeight);
+                        clearTimeout(timer);
+                        timer = setTimeout(function() {
+                            that.clearHeight();
+                        }, 250);
+                        break;
+                }
+
+                that.update();
+            });
+        }
     };
 
     module.exports = {
         Scene: Scene,
         Role: Role,
+        RoleSwitch: RoleSwitch,
         Extension: Extension
     };
 });
