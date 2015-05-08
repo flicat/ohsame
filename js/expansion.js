3b42102cf02ca07524502459428cf07ebc671e86
flicat <liyuelong1020@gmail.com>
2015-05-08 15:23:34 +0800

    add files

--- js/expansion.js
+++ js/expansion.js
@@ -9,13 +9,58 @@ define(function(require, exports, module) {
     var zepto = require('zepto');
     var scene = require('scene');
 
+    var url = 'http://192.168.0.206:4001/ohsame';
+
+    /** @description 需要加载的图片资源 */
+    var images = [
+        url + "/images/best-bg.jpg",
+        url + "/images/share-bg.png",
+        url + "/images/bg.png",
+        url + "/images/cloud.png",
+        url + "/images/tip.gif",
+        url + "/images/btn-question.png",
+        url + "/images/btn-love.png",
+        url + "/images/prize.png",
+        url + "/images/gf-logo.png",
+        url + "/images/thumb-1-gray.jpg",
+        url + "/images/thumb-2-gray.jpg",
+        url + "/images/thumb-3-gray.jpg",
+        url + "/images/thumb-4-gray.jpg",
+        url + "/images/thumb-1.jpg",
+        url + "/images/thumb-2.jpg",
+        url + "/images/thumb-3.jpg",
+        url + "/images/thumb-4.jpg",
+        url + "/images/chimney-1.png",
+        url + "/images/chimney-2.png",
+        url + "/images/chimney-3.png",
+        url + "/images/chimney-4.png",
+        url + "/images/chimney-layout-1.png",
+        url + "/images/chimney-layout-2.png",
+        url + "/images/chimney-layout-3.png",
+        url + "/images/chimney-layout-4.png",
+        url + "/images/role-1-head.png",
+        url + "/images/role-1-neck.png",
+        url + "/images/role-1-body.png",
+        url + "/images/role-2-head.png",
+        url + "/images/role-2-neck.png",
+        url + "/images/role-2-body.png",
+        url + "/images/role-3-head.png",
+        url + "/images/role-3-neck.png",
+        url + "/images/role-3-body.png",
+        url + "/images/role-4-head.png",
+        url + "/images/role-4-neck.png",
+        url + "/images/role-4-body.png"
+    ];
+
     /** @description 动画节点 */
     var chimneyNodes = {
         chimney: $('#chimney'),
         cloud: $('#cloud'),
         score: {
             currentScore: $('#current-score'),
-            bestScore: $('#best-score')
+            bestScore: $('#best-score'),
+            tips: $('#tips'),
+            collect: $('#collect')
         },
         roleList: {
             roleList: $('#role-list'),
@@ -49,66 +94,152 @@ define(function(require, exports, module) {
             $('#role-icon-2'),
             $('#role-icon-3'),
             $('#role-icon-4')
-        ]
+        ],
+        popup: {
+            prize: $('#prize'),
+            tipQuestion: $('#tip-question'),
+            tipScore: $('#tip-score'),
+            lastScore: $('#last-score'),
+            btnShare: $('#btn-share'),
+            share: $('#share'),
+            loading: $('#loading')
+        }
     };
 
-    /**
-     * @param {Node} background
-     * @param {Node} cloud
-     * @returns {Object} 场景对象
-     * @description 场景
-     */
-    var Scene = new scene.Scene(chimneyNodes.chimney, chimneyNodes.cloud);
+    // 资源加载
+    $.when.apply($, $.map(images, function(url) {
+            return $.get(url);
+        })).then(function() {
 
-    /**
-     * @param {Node} role
-     * @param {Node} head
-     * @param {Node} neck
-     * @param {Node} body
-     * @returns {Object} 角色对象
-     * @description 角色列表
-     */
-    var roleList = [
-        new scene.Role(
-            chimneyNodes.roleList.role_1.role,
-            chimneyNodes.roleList.role_1.head,
-            chimneyNodes.roleList.role_1.neck,
-            chimneyNodes.roleList.role_1.body
-        ),
-        new scene.Role(
-            chimneyNodes.roleList.role_2.role,
-            chimneyNodes.roleList.role_2.head,
-            chimneyNodes.roleList.role_2.neck,
-            chimneyNodes.roleList.role_2.body),
-        new scene.Role(
-            chimneyNodes.roleList.role_3.role,
-            chimneyNodes.roleList.role_3.head,
-            chimneyNodes.roleList.role_3.neck,
-            chimneyNodes.roleList.role_3.body),
-        new scene.Role(
-            chimneyNodes.roleList.role_4.role,
-            chimneyNodes.roleList.role_4.head,
-            chimneyNodes.roleList.role_4.neck,
-            chimneyNodes.roleList.role_4.body)
-    ];
+            // 点击关闭 popup
+            chimneyNodes.popup.tipQuestion.on('click', function() {
+                $(this).hide();
+            });
+            chimneyNodes.popup.share.on('click', function() {
+                $(this).hide();
+            });
+            chimneyNodes.popup.tipScore.on('click', function() {
+                $(this).hide();
+            });
 
-    /**
-     * @param {[Role]} roleArr
-     * @param {NodeList} roleIconNodeArr
-     * @param {Node} roleList
-     * @returns {Object} 角色切换方法
-     * @description 角色切换
-     */
-    var RoleSwitch = new scene.RoleSwitch(roleList, chimneyNodes.roleIconList, roleList);
+            // 点击获取礼物
+            chimneyNodes.popup.prize.on('click', function() {
+                alert('prize !');
+                $(this).hide();
+            });
 
-    /**
-     * @param {Scene} scene
-     * @param {RoleSwitch} role
-     * @returns {Object} 拉长脖子对象
-     * @description 拉长脖子
-     * @constructs
-     */
-    var Extension = new scene.Extension(Scene, RoleSwitch);
+            // 点击显示 tip
+            chimneyNodes.score.tips.on('click', function() {
+                chimneyNodes.popup.tipQuestion.show();
+            });
+
+            // 点击显示分享提示
+            chimneyNodes.popup.btnShare.on('click', function() {
+                chimneyNodes.popup.tipScore.hide();
+                chimneyNodes.popup.share.show();
+            });
+            chimneyNodes.score.collect.on('click', function() {
+                chimneyNodes.popup.share.show();
+            });
+
+            // 关闭 loading
+            chimneyNodes.popup.loading.hide();
+
+            // 默认显示 tips
+            chimneyNodes.popup.tipQuestion.show();
+
+
+            /**
+             * @param {Node} background
+             * @param {Node} cloud
+             * @returns {Object} 场景对象
+             * @description 场景
+             */
+            var Scene = new scene.Scene(chimneyNodes.chimney, chimneyNodes.cloud);
+
+            /**
+             * @param {Node} role
+             * @param {Node} head
+             * @param {Node} neck
+             * @param {Node} body
+             * @returns {Object} 角色对象
+             * @description 角色列表
+             */
+            var roleList = [
+                new scene.Role(
+                    chimneyNodes.roleList.role_1.role,
+                    chimneyNodes.roleList.role_1.head,
+                    chimneyNodes.roleList.role_1.neck,
+                    chimneyNodes.roleList.role_1.body
+                ),
+                new scene.Role(
+                    chimneyNodes.roleList.role_2.role,
+                    chimneyNodes.roleList.role_2.head,
+                    chimneyNodes.roleList.role_2.neck,
+                    chimneyNodes.roleList.role_2.body),
+                new scene.Role(
+                    chimneyNodes.roleList.role_3.role,
+                    chimneyNodes.roleList.role_3.head,
+                    chimneyNodes.roleList.role_3.neck,
+                    chimneyNodes.roleList.role_3.body),
+                new scene.Role(
+                    chimneyNodes.roleList.role_4.role,
+                    chimneyNodes.roleList.role_4.head,
+                    chimneyNodes.roleList.role_4.neck,
+                    chimneyNodes.roleList.role_4.body)
+            ];
+
+            /**
+             * @param {[Role]} roleArr
+             * @param {NodeList} roleIconNodeArr
+             * @param {Node} roleList
+             * @returns {Object} 角色切换方法
+             * @description 角色切换
+             */
+            var RoleSwitch = new scene.RoleSwitch(roleList, chimneyNodes.roleIconList, chimneyNodes.roleList.roleList);
+
+            /**
+             * @param {Scene} scene
+             * @param {RoleSwitch} role
+             * @returns {Object} 拉长脖子对象
+             * @description 拉长脖子
+             * @constructs
+             */
+            var Extension = new scene.Extension(Scene, RoleSwitch);
+            // 更新分数
+            var bestScore = 0, oldBestScore = 0, prizeToggle = false;
+            // 分数节点
+            var currentScoreNode = chimneyNodes.score.currentScore,
+                bestScoreNode = chimneyNodes.score.bestScore;
+            // 实时更新分数
+            Extension.onUpdate = function(interval) {
+                interval = parseInt(interval / 10);
+                interval < -5 && (interval = -5);
+                currentScoreNode.html(interval);
+                // 创造了新记录
+                if(interval > bestScore){
+                    bestScore = interval;
+                    bestScoreNode.html(bestScore);
+                }
+                // 超过 200m 则显示礼物
+                if(interval >= 200 && prizeToggle === false){
+                    chimneyNodes.popup.prize.show();
+                    prizeToggle = true;
+                }
+            };
+            // 最终结果
+            Extension.onEnd = function() {
+                if(oldBestScore < bestScore && bestScore >= 40){
+                    oldBestScore = bestScore;
+                    // 显示最佳成绩
+                    chimneyNodes.popup.lastScore.html(bestScore);
+                    setTimeout(function() {
+                        chimneyNodes.popup.tipScore.show();
+                    }, 600);
+                }
+            };
+
+        });
 
 });
 
--- js/scene.js
+++ js/scene.js
@@ -14,6 +14,10 @@ define(function(require, exports, module) {
     var winHeight = window.innerHeight;
     /** @description 头距离屏幕上方的高度 */
     var offsetTop = 0;
+    // 返回给定范围内的随机整数
+    var getRandomInt = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
 
     /**
      * @param {Node} background
@@ -25,24 +29,66 @@ define(function(require, exports, module) {
     var Scene = function(background, cloud) {
         this.background = background;        // 背景层
         this.cloud = cloud;                  // 白云层
-        this.initCloud();
+        this.cloudArr = [];
+        this.init();
     };
     Scene.prototype = {
         constructor: Scene,
         // 更新背景高度
-        scrollBackground: function(metre) {
+        scrollBackground: function(interval) {
+            // 超出屏幕则不再伸缩
+            var metre = interval > winHeight ? winHeight : interval;
+
             metre -= offsetTop;
             if(metre < 0){
                 metre = 0;
             }
             this.background.css({
-                '-webkit-transform': 'translateY(' + metre + 'px)',
-                'transform': 'translateY(' + metre + 'px)'
+                '-webkit-transform': 'translate3d(0, ' + metre + 'px, 0)',
+                'transform':         'translate3d(0, ' + metre + 'px, 0)'
+            });
+            this.scrollCloud(interval);
+        },
+        // 更新白云
+        scrollCloud: function(interval) {
+            var that = this;
+            interval -= offsetTop;
+            if(interval < 0){
+                interval = 0;
+            }
+            $.each(that.cloudArr, function(i, cloud) {
+                var diff = (interval / 3 + cloud.cssTop + cloud.height) % winHeight - cloud.height;
+                cloud.css({
+                    '-webkit-transform': 'translate3d(0, ' + diff + 'px, 0)',
+                    'transform':         'translate3d(0, ' + diff + 'px, 0)'
+                });
             });
         },
         // 初始化白云
-        initCloud: function() {
-
+        init: function() {
+            var cloudItem = this.cloud.children();
+            for(var i = 0; i < 16; i++){
+                var cloud = cloudItem.clone();
+                var top = getRandomInt(1, winHeight);           // 高度
+                var delay = getRandomInt(0, 10);      // 动画延迟
+                var speed = getRandomInt(20, 80);                 // 运动速度
+                var opacity = getRandomInt(30, 90) / 100;           // 透明度
+                var height = getRandomInt(10, 30) / 10;             // 大小
+                cloud.cssTop = top;
+                cloud.height = height * 16;
+                cloud.css({
+                    '-webkit-transform': 'translate3d(0, ' + top + 'px, 0)',
+                    'transform':         'translate3d(0, ' + top + 'px, 0)',
+                    'height': height + 'rem'
+                });
+                cloud.children().css({
+                    'opacity': opacity,
+                    '-webkit-animation': 'cloud ' + speed + 's linear ' + delay + 's infinite',
+                    'animation':         'cloud ' + speed + 's linear ' + delay + 's infinite'
+                });
+                this.cloudArr.push(cloud);
+                this.cloud.append(cloud);
+            }
         }
     };
 
@@ -61,33 +107,69 @@ define(function(require, exports, module) {
         this.neck = neck;                     // 脖子
         this.body = body;                     // 身体
         this.neckHeight = neck.height();      // 脖子高度
-        this.offsetTop = role.offset().top - 100;   // 头距离屏幕上方的高度
+        this.offsetTop = role.offset().top - role.height() - 100;   // 头距离屏幕上方的高度
+        this.isStop = true;                 // 是否停止拉伸
     };
-    // 脖子拉长方法
-    Role.prototype.expansion = function(interval) {
-        // 脖子伸缩量
-        var scaleDiff = interval / this.neckHeight;
-        if(scaleDiff <= 0){
-            scaleDiff = 1;
-        }
-        // 头位移
-        var translateDiff = interval - this.neckHeight;
-        if(scaleDiff === 1) {
-            translateDiff = 0;
-        }
-//        console.log(interval, this.neckHeight, scaleDiff);
+    Role.prototype = {
 
-        // 头部上移
-        this.head.css({
-            '-webkit-transform': 'translateY(-' + translateDiff + 'px)',
-            'transform': 'translateY(-' + translateDiff + 'px)'
-        });
-        // 伸缩脖子
-        this.neck.css({
-            '-webkit-transform': 'scaleY(' + scaleDiff + ')',
-            'transform': 'scaleY(' + scaleDiff + ')'
-        });
+        constructor: Role,
 
+        // 初始化脖子晃动效果
+        shake: function() {
+            this.isStop = true;
+            // 复原头部和脖子
+            this.head.css({
+                '-webkit-transform': 'translate3d(0,0,0)',
+                'transform':         'translate3d(0,0,0)'
+            });
+            this.neck.css({
+                '-webkit-transform': 'scaleY(1)',
+                'transform':         'scaleY(1)'
+            });
+            // 添加晃动动画
+            this.role.addClass('shake-animation');
+        },
+
+        // 脖子拉长方法
+        expansion: function(interval) {
+            // 脖子伸缩量，头部偏移量
+            var scaleDiff = 1, translateDiff = 0;
+
+            // 超出屏幕则不再伸缩
+            var diff = interval > winHeight ? winHeight : interval;
+
+            if(diff === 0){
+                // 如果是复原脖子则添加晃动效果
+                this.shake();
+            } else {
+                if(diff > 0){
+                    // 往上拉伸
+                    scaleDiff = (diff + this.neckHeight) / this.neckHeight;
+                    translateDiff = -diff;
+                } else {
+                    // 往下拉伸
+                    scaleDiff = ((diff / 10 + this.neckHeight) / this.neckHeight).toFixed(3);
+                    scaleDiff < 0 && (scaleDiff = 0);
+                    translateDiff = this.neckHeight - Math.ceil(this.neckHeight * scaleDiff);
+                }
+                // 停止晃动动画
+                if(this.isStop) {
+                    this.role.removeClass('shake-animation');
+                    this.isStop = false;
+                }
+                // 头部上移
+                this.head.css({
+                    '-webkit-transform': 'translate3d(0,' + translateDiff + 'px,0)',
+                    'transform':         'translate3d(0,' + translateDiff + 'px,0)'
+                });
+                // 伸缩脖子
+                this.neck.css({
+                    '-webkit-transform': 'scaleY(' + scaleDiff + ')',
+                    'transform':         'scaleY(' + scaleDiff + ')'
+                });
+            }
+
+        }
     };
 
     /**
@@ -102,16 +184,47 @@ define(function(require, exports, module) {
         this.roleArr = roleArr;             // 角色列表
         this.iconArr = roleIconNodeArr;     // 角色切换按钮列表
         this.roleList = roleList;     // 角色切换 ul 节点
-        this.currentRole = roleArr[0];      // 当前角色
-        offsetTop = this.currentRole.offsetTop;   // 头距离屏幕上方的高度
+        this.init();
     };
     RoleSwitch.prototype = {
         constructor: RoleSwitch,
         // 切换角色
-        switchRole: function() {},
+        switchRole: function(index) {
+            var diff = index * -25;
+            if(this.currentRole){
+                // 隐藏旧角色
+                this.currentRole.role.css({
+                    '-webkit-transform': 'translate3d(0,100%,0)',
+                    'transform':         'translate3d(0,100%,0)'
+                });
+            }
+            // 当前角色
+            this.currentRole = this.roleArr[index];
+            this.currentRole.role.css({
+                '-webkit-transform': 'translate3d(0,0,0)',
+                'transform':         'translate3d(0,0,0)'
+            });
+            // 切换效果
+            this.roleList.css({
+                '-webkit-transform': 'translate3d(' + diff + '%,0,0)',
+                'transform':         'translate3d(' + diff + '%,0,0)'
+            });
+            // 头距离屏幕上方的高度
+            offsetTop = this.currentRole.offsetTop;
+        },
         // 绑定点击事件
         init: function() {
-
+            var that = this;
+            var currentIcon = that.iconArr[0];
+            this.switchRole(0);
+            $.each(that.iconArr, function(index, icon) {
+                icon.on('touchend', function() {
+                    currentIcon.removeClass('current');
+                    icon.addClass('current');
+                    currentIcon = icon;
+                    that.switchRole(index);
+                })
+            });
         }
     };
 
@@ -127,6 +240,8 @@ define(function(require, exports, module) {
         this.role = role;        // 角色列表
         this.initHeight = 0;     // 初始化缩放高度
         this.initPointY = 0;
+        this.onUpdate = function() {};    // 更新分数事件
+        this.onEnd = function() {};       // 游戏结束
         this.init();
     };
     Extension.prototype = {
@@ -142,6 +257,8 @@ define(function(require, exports, module) {
             this.role.currentRole.expansion(interval);
             // 场景更新
             this.scene.scrollBackground(interval);
+            // 更新事件
+            this.onUpdate(interval);
         },
 
         // 初始化偏移量
@@ -149,42 +266,55 @@ define(function(require, exports, module) {
             this.initPointY = 0;
             this.initHeight = 0;
             this.update();
+            this.onEnd();
         },
 
         // 绑定触摸事件
         init: function() {
             var that = this;
             // 触摸延时清除
-            var timer = null;
+            var resetTimer = null, animationTimer = null;
+            // 是否滑动
+            var isMove = 0;
             // 绑定触摸事件
-            $(document).on('touchstart touchmove touchend touchcancel', function(e) {
+            that.scene.background.on('touchstart touchmove touchend touchcancel', function(e) {
                 e.preventDefault();
                 e.stopPropagation();
-//                console.log(e);
 
                 // 触摸坐标点
                 var points = e.touches[0];
 
                 switch(e.type) {
                     case 'touchstart':
-//                        clearTimeout(timer);
                         that.initPointY = points.clientY;
                         break;
                     case 'touchmove':
-                        that.initHeight += that.initPointY - points.clientY;
-                        that.initPointY = points.clientY;
+                        // 设置更新频率
+                        if(!animationTimer){
+                            animationTimer = setTimeout(function() {
+                                that.initHeight += that.initPointY - points.clientY;
+                                that.initPointY = points.clientY;
+                                that.update();
+                                clearTimeout(animationTimer);
+                                animationTimer = null;
+                            }, 1000/60);
+                            isMove = 1;
+                        }
                         break;
                     case 'touchend':
                     case 'touchcancel':
-//                        console.log(that.initHeight);
-                        clearTimeout(timer);
-                        timer = setTimeout(function() {
-                            that.clearHeight();
-                        }, 250);
+                        if(isMove) {
+                            clearTimeout(resetTimer);
+                            resetTimer = setTimeout(function() {
+                                clearTimeout(animationTimer);
+                                animationTimer = null;
+                                that.clearHeight();
+                            }, 300);
+                            isMove = 0;
+                        }
                         break;
                 }
 
-                that.update();
             });
         }
     };
